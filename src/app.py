b5e496a73d25b1ce6085da92c2707ff2929422f3
ayedaemon <ris3234@gmail.com>
2022-07-01 14:50:34 +0530

    4

--- src/app.py
+++ src/app.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 from os import environ
-from json import load
+from json import loads
 
 
-GITHUB_EVENT_PATH = environ.get('GITHUB_EVENT_PATH', 'dummy-event')
+GITHUB_EVENT_PATH = environ.get('GITHUB_EVENT_PATH', './test/event.json')
 ACCESS_TOKEN = environ.get('ACCESS_TOKEN', 'dummy-token')
 
 print(GITHUB_EVENT_PATH, ACCESS_TOKEN)
 
 
 
-with open(GITHUB_EVENT_PATH) as event_file:
-    event = load(event_file.read())
+with open(GITHUB_EVENT_PATH, "r") as event_file:
+    event = loads(event_file.read())
 
 
 print(type(event), event)
